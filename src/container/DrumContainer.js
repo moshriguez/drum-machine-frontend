511d95cb64a69334dfb06f241a321a0d1626b896
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-02 11:43:35 -0700

    basic layout for drum machine complete

--- src/container/DrumContainer.js
+++ src/container/DrumContainer.js
@@ -5,9 +5,17 @@ const DrumContainer = () => {
     return (
         <div className="drum-container">
             <div className="global-controls">
-                <div className="tempo-control"></div>
+                <div className="tempo-control">
+                    <div>
+                        <label htmlFor="tempo">TEMPO</label>
+                        <div className="digital-display">
+                            <p>280</p>
+                        </div>
+                    </div>
+                    <input name="tempo" id="tempo" type="range" min="40" max="280" step="0.5" />
+                </div>
                 <div className="play-btn">
-                    <p>Play</p>
+                    <p>PLAY</p>
                 </div>
             </div>
             <div className="pads-container">
@@ -22,7 +30,10 @@ const DrumContainer = () => {
                         <p>Pad1</p>
                     </div>
                 </div>
-                <div className="volume-control"></div>
+                <div className="volume-control">
+                    <label htmlFor="volume">VOLUME</label>
+                    <input name="volume" id="volume" type="range" min="0" max="2" step="0.01" />
+                </div>
             </div>
             <div className="pads-container">
                 <div className="sequence-pad"></div>

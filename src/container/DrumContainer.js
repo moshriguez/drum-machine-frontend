90e5002e63c2d04b4469874b2450d9086fc18dbb
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-02 10:04:12 -0700

    basic play btn and digital display

--- src/container/DrumContainer.js
+++ src/container/DrumContainer.js
@@ -6,7 +6,9 @@ const DrumContainer = () => {
         <div className="drum-container">
             <div className="global-controls">
                 <div className="tempo-control"></div>
-                <div className="play-btn"></div>
+                <div className="play-btn">
+                    <p>Play</p>
+                </div>
             </div>
             <div className="pads-container">
                 <div className="drum-pad"></div>
@@ -15,7 +17,11 @@ const DrumContainer = () => {
                 <div className="drum-pad"></div>
             </div>
             <div className="drum-controls">
-                <div className="selected-drum-show"></div>
+                <div className="selected-drum-show">
+                    <div className="digital-display">
+                        <p>Pad1</p>
+                    </div>
+                </div>
                 <div className="volume-control"></div>
             </div>
             <div className="pads-container">

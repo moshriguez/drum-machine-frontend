82c7d605b0883ca734ead2fe451bfebaada0e1bc
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-02 15:25:02 -0700

    DM displays selected pad in display

--- src/container/DrumContainer.js
+++ src/container/DrumContainer.js
@@ -52,7 +52,7 @@ const DrumContainer = () => {
             <div className="drum-controls">
                 <div className="selected-drum-show">
                     <div className="digital-display">
-                        <p>Pad1</p>
+                        <p>{selectedPad}</p>
                     </div>
                 </div>
                 <div className="volume-control">

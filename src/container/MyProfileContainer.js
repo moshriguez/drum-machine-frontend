d39f7deab0dc83b297694abfa50f10325b8d8d38
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-05 14:20:33 -0700

    basic layout for profile page

--- src/container/MyProfileContainer.js
+++ src/container/MyProfileContainer.js
@@ -1,11 +1,24 @@
 import React from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+
+
 const MyProfileContainer = () => {
     const user = useSelector(state => state.user)
 
     return (
         <div className="my-profile-container">
-            <p>profile container</p>
+            <div className="profile-section">
+                <h2>User: {user.username}</h2>
+                <h3>Bio:</h3>
+                <p>{user.bio}</p>
+            </div>
+            <div className="feed-section">
+                <h2>Feed:</h2>
+            </div>
+            <div className="beats-section">
+                <h2>Beats:</h2>
+            </div>
         </div>
     )
 }

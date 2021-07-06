8f11c158c65abfdf4fded90b5d8a34429e6d6c26
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-06 15:31:50 -0700

    beatcard has linkks to beat

--- src/components/BeatCard.js
+++ src/components/BeatCard.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+
 
 const BeatCard = ({ beat }) => {
 
     return (
         <div className="beat-card">
-            <h4>{beat.name}</h4>
+            <Link to={'/drum_machine/' + beat.id}>{beat.name}</Link>
             <p>{beat.description}</p>
             <span>Tempo: {beat.tempo}</span>
         </div>

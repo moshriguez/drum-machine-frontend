96708e6f74541416b28428fea31d7e436c10d7f3
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-07 11:48:39 -0700

    added conditionally rendered username

--- src/components/BeatCard.js
+++ src/components/BeatCard.js
@@ -7,6 +7,7 @@ const BeatCard = ({ beat }) => {
     return (
         <div className="beat-card">
             <Link to={'/drum_machine/' + beat.id}>{beat.name}</Link>
+            {beat.user ? <Link to={'/profile/' + beat.user.id}>{beat.user.username}</Link> : null}
             <p>{beat.description}</p>
             <span>Tempo: {beat.tempo}</span>
         </div>
